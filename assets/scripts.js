de4f14ec03b5fa6d96e5f806937d62bf0f0b4cd5
Heat Lai <heatlai@core-tech.tw>
2018-10-19 16:46:58 +0800

    .

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,9 +2,9 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-$(function (){
-    if ( window.location && location.protocol !== 'https:')
-    {
-        location = 'https://'+location.host+location.pathname;
-    }
-});
+// $(function (){
+//     if ( window.location && location.protocol !== 'https:')
+//     {
+//         location = 'https://'+location.host+location.pathname;
+//     }
+// });
